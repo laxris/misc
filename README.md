5a45acb4d4568a1df7ad2887468333b1f8d9306d
Chris Hammerschmidt <laxris@gmail.com>
2014-08-08 15:47:32 +0200

    first setup of repo

---
+++ README.md
@@ -0,0 +1,3 @@
+I used to not have my own repositories on github---mostly because I did not want to put more unmaintained code out there. A lot of things tend to break or at least need some updating over time, as the build tools, APIs, or underlying frameworks evolve.
+
+This repo just serves to show that I can write code and know how to use standard development tools.
---
+++ hackerrank/easy_sum.java
@@ -0,0 +1,32 @@
+﻿/* My solution to				  *
+ * https://www.hackerrank.com/challenges/easy-sum *
+ *						  */
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+    static long easySum(long N, long m) {   
+            return ((m-1)*m/2)*(N/m) + (N%m)*((N%m)+1)/2;
+   }
+
+   
+ public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int _cases;
+        _cases = in.nextInt();
+     
+        for(int i=0; i<_cases; i++) {
+            long _N, _m;
+            _N = in.nextLong();
+            _m = in.nextLong();
+         
+            System.out.println(easySum(_N, _m));
+        }     
+   }
+}
