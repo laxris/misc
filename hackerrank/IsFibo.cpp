67e73d7f58e5746a2bf8951f2f730e718bd11c6d
Chris Hammerschmidt <laxris@gmail.com>
2014-08-11 14:09:22 +0200

    added isfibo

---
+++ hackerrank/IsFibo.cpp
@@ -0,0 +1,32 @@
+﻿// https://www.hackerrank.com/challenges/is-fibo
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+bool isFibo(long a) {
+  return  (fmod((sqrt(5*pow(a,2) + 4)),1) == 0) || (fmod((sqrt(5*pow(a,2) - 4)),1) == 0);
+}
+
+int main() {
+  int _cycles;
+  long number;
+  cin>>_cycles;
+    
+  for(int i=0;i<_cycles;i++) {
+    cin>>number;
+    if(isFibo(number)) {
+          cout<<"IsFibo";
+    }
+    else {
+          cout<<"IsNotFibo";
+    }
+   cout<<'\n';
+  }
+    
+  return 0;
+}
---
+++ hackerrank/IsFibo.java
@@ -0,0 +1,30 @@
+﻿// https://www.hackerrank.com/challenges/is-fibo
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+    
+ public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int _cases;
+        _cases = in.nextInt();
+     
+        long n;
+     
+        for(int i=0; i<_cases; i++) {
+            
+            n = in.nextLong();
+
+            if(Math.sqrt(5*Math.pow(n,2) + 4) % 1 == 0 || Math.sqrt(5*Math.pow(n,2) - 4) %1 == 0)
+                System.out.println("IsFibo");
+            else
+                System.out.println("IsNotFibo");
+        }     
+   }
+}
